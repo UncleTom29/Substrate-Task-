b398cca21cc9fe50f2edd9086ef6abd544d1b270
Tomiwa Adeyemi <adeyemitomiwa7@gmail.com>
2022-09-22 10:40:02 +0100

    Callable function (add_to_awaitinglist) implemented

--- pallets/RotaryClub/src/lib.rs
+++ pallets/RotaryClub/src/lib.rs
@@ -89,24 +89,35 @@ pub mod pallet {
 
 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
 	// These functions materialize as "extrinsics", which are often compared to transactions.
-	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
+
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
+		pub fn add_to_awaitinglist(origin: OriginFor<T>) -> DispatchResult {
 			// Check that the extrinsic was signed and get the signer.
 			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/main-docs/build/origins/
-			let who = ensure_signed(origin)?;
+	
+			let applicant = ensure_signed(origin)?;
 
-			// Update storage.
-			<Something<T>>::put(something);
+			ensure!(
+				<AwaitingOriginApproval<T>>::get().contains(&applicant) == false,
+				<Error<T>>::AlreadyOnAwaitingList
+			);
+			ensure!(
+				<InitialMembers<T>>::get().contains(&applicant) == false,
+				<Error<T>>::RotaryClubMemberAlready
+			);
+
+			<AwaitingOriginApproval<T>>::try_mutate(|b_vec| b_vec.try_push(applicant.clone()))
+				.map_err(|_| <Error<T>>::AwaitingListFull)?;
+
+			<InitialMembers<T>>::try_mutate(|b_vec| b_vec.try_push(applicant.clone()))
+				.map_err(|_| <Error<T>>::RotaryClubFull)?;
 
 			// Emit an event.
-			Self::deposit_event(Event::SomethingStored(something, who));
-			// Return a successful DispatchResultWithPostInfo
+			Self::deposit_event(Event::AwaitingListMemberAdded(applicant));
+
 			Ok(())
 		}
 
